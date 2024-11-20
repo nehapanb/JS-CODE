9c43a7d3f6b149bff06a8a7ce1c238ad1e6f7b03
Neha panbude <nehapanbude123@gmail.com>
2024-11-20 12:29:04 +0530

    first commit

--- jason18/index.js
+++ jason18/index.js
@@ -8,9 +8,23 @@ async function fet_data(){
     <td>${t.name}</td>
     <td>${t.age}</td>
     <td>${t.address}</td>
+    <td> <button onclick="mydelete('${t.id}')"> Delete </button> </td>
     </tr>
+
     `).join("")
+
     document.querySelector('#show').innerHTML= final_data
 }
   fet_data();
+
+  function mydelete(id){
+    fetch(`http://localhost:3000/students/${id}`,{
+    
+      method:'DELETE'
+  
+        
+      })
+      .then(re=>alert("deleted successfully..!!!"))
+    }
+  
 
