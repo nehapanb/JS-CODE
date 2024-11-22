ae21c58fabf817da3334fa590cb90c79a0743b43
Neha panbude <nehapanbude123@gmail.com>
2024-11-22 12:22:00 +0530

    second commit

--- jason18/index.js
+++ jason18/index.js
@@ -9,6 +9,7 @@ async function fet_data(){
     <td>${t.age}</td>
     <td>${t.address}</td>
     <td> <button onclick="mydelete('${t.id}')"> Delete </button> </td>
+    <td> <button onclick="myedit('${t.id}')"> edit</button> </td>
     </tr>
 
     `).join("")
@@ -26,5 +27,55 @@ async function fet_data(){
       })
       .then(re=>alert("deleted successfully..!!!"))
     }
+    function insertdata(){
+      let data = {
+          name : document.querySelector('#name').value,
+          age : document.querySelector('#age').value,
+          address : document.querySelector('#address').value
+      }
+      fetch("http://localhost:3000/students",{
+          method : 'POST',
+          Headers:{
+              'content-type':'application/json'
+          },
+          body:JSON.stringify(data)
+  
+          })
+          .then(res=>alert("inserted..!!!"))
+  }      
+  async function myedit(id) {
+      let myupdate = await fetch(`http://localhost:3000/students/${id}`)
+      let redata = await myupdate.json()
+  
+      let senddata =`
+      <input type="text" value="${redata.id}"readonly id="id1"><br>
+      <input type="text" value="${redata.name}" id="name1"><br>
+  <input type="text" value="${redata.age}" id="age1"><br>
+  <input type="text" value="${redata.address}" id="address1"><br>
+          <input type="submit" onclick="finalupdate('${redata.id}')">
+  
+          `
+          document.querySelector('#edittable').innerHTML = senddata
+  }
+  function finalupdate(id){
+    let fdata={
+        name:document.querySelector('#name1').value,
+        age:document.querySelector('#age1').value,
+        address:document.querySelector('#address1').value,
+
+    }
+    fetch(`http://localhost:3000/students/${id}`,{
+        method:'PUT',
+        
+        headers:{
+            'content-type':'application/json'
+
+        },
+        body:JSON.stringify(fdata)
+
+    })
+    .then(r=>alert("updated..!!!"))
+    
+  }
   
 
--- jason18/insert.js
+++ jason18/insert.js
@@ -13,6 +13,38 @@ function insertdata(){
 
         })
         .then(res=>alert("inserted..!!!"))
-    }
+}      
+async function myedit(id) {
+    let myupdate = await fetch(`http://localhost:3000/students\${id}`)
+    let redata = await myupdate.json()
 
+    let senddata =`
+    <input type="text" value="${redata.id}"readonly><br>
+    <input type="text" value="${redata.name}"readonly><br>
+<input type="text" value="${redata.age}"readonly><br>
+<input type="text" value="${redata.address}"readonly><br>
+        <input type="submit" onclick="finalupdate"('${redata.id}')">
+
+        `
+        document.querySelector('#edittable').innerHTML = senddata
+}
+function finalupdate(id){
+    let fdata={
+        name:document.querySelector('#name1').value,
+        age:document.querySelector('#age1').value,
+        address:document.querySelector('#address1').value,
+
+    }
+    fet_data(`/${id}`,{
+        method:'PUT',
+        
+        headers:{
+            'content-type':'application/json'
+
+        },
+        body:JSON.stringify(fdata)
+
+    })
+    .then(r=>alert("updated..!!!"))
     
+}
